f6f4ffdf36ec9641c59351415a24fa3eec547bb9
Amir Sanati <38419705+mramirsnt@users.noreply.github.com>
2025-02-07 17:42:09 +0330

    Add files via upload

---
+++ python/outoftree/bindings/docstrings/Z_ScoreC_pydoc_template.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr,outoftree, __VA_ARGS__ )
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+ 
+ static const char *__doc_gr_outoftree_Z_ScoreC = R"doc()doc";
+
+
+ static const char *__doc_gr_outoftree_Z_ScoreC_Z_ScoreC = R"doc()doc";
+
+
+ static const char *__doc_gr_outoftree_Z_ScoreC_make = R"doc()doc";
+
+  
